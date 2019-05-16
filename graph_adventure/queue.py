4d4f7224398648dbc3af9ee73a99ca5144ac2a8a
johnoro <officialjohnorourke@gmail.com>
2019-05-16 19:11:55 -0400

    Finishes initial implementation

--- graph_adventure/queue.py
+++ graph_adventure/queue.py
@@ -1,18 +1,20 @@
+from collections import deque
+
 class Queue:
   def __init__(self, start=None):
     if start is None:
-      self.queue = []
+      self.data = deque()
     else:
-      self.queue = [start]
+      self.data = deque([start])
   
   def __len__(self):
-    return len(self.queue)
+    return len(self.data)
   
   def enqueue(self, value):
-    self.queue.append(value)
+    self.data.append(value)
   
   def dequeue(self):
     if len(self) > 0:
-      return self.queue.pop(0)
+      return self.data.popleft()
     else:
       return None
